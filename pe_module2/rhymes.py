3f0021f3c9f983656fbc0789950101ad2c5c0b7d
Brian Lucius <brian@brianlucius.net>
2023-01-02 14:53:53 -0700

    Module 2 Exercises

---
+++ pe_module2/rhymes.py
@@ -0,0 +1,16 @@
+print("The itsy bitsy spider\nclimbed up the waterspout.")
+print()
+print("Down came the rain\nand washed the spider out.\n")
+
+print("The itsy bitsy spider", "climbed up", "the waterspout.\n")
+
+print("My name is", "Python.")
+print("Monty Python.\n")
+
+print("My name is", "Python.", end=" ")
+print("Month Python.\n")
+
+print("My", "name", "is", "Monty", "Python.\n", sep="-")
+
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
