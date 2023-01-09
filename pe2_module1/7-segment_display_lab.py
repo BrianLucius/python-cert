538c94a3ffbe8f945dd79d55f444cdd0d07cd6b1
Brian Lucius <brian@brianlucius.net>
2023-01-08 22:36:04 -0700

    Improved input handling

--- pe2_module1/7-segment_display_lab.py
+++ pe2_module1/7-segment_display_lab.py
@@ -11,16 +11,18 @@ digits = [['###','# #','# #','# #','###'],
 
 def display_digits(number):
     for i in range(5):
-        for d in str(number):
-            print(digits[int(d)][i], end=' ')
+        for d in number:
+            print(digits[int(d)][i], end='  ')
         print('')
 
-entry = 0
-while entry >= 0:
+entry = ''
+while entry.upper() != 'Q':
     try:
-        entry = int(input("Enter a positive integer to display, or a negative value to exit: "))
-        if entry >= 0:
+        entry = input("Enter a positive integer to display or Q to exit: ")
+        if entry.isnumeric():
             display_digits(entry)
+        elif entry.upper() != 'Q':
+            print("Invalid input. Try again.")
     except:
         print("Invalid value entered. Try again.")
 
